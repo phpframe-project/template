43ed8aa7d601477184f8d6f3ce90447022dbe6f3
bobo <bobo@local>
2026-01-18 12:57:40 +0800

    remove unused route

--- routes/shell.php
+++ routes/shell.php
@@ -3,13 +3,8 @@ use PHPFrame\Facades\Route;
 
 Route::shell('default/test', [App\Controllers\Shell\DefaultShell::class, 'testAction']);
 
-Route::shell('database/migrate', [App\Controllers\Shell\DatabaseShell::class, 'migrateAction']);
-Route::shell('database/rollback', [App\Controllers\Shell\DatabaseShell::class, 'rollbackAction']);
-Route::shell('database/reset', [App\Controllers\Shell\DatabaseShell::class, 'resetAction']);
-Route::shell('database/refresh', [App\Controllers\Shell\DatabaseShell::class, 'refreshAction']);
-Route::shell('database/status', [App\Controllers\Shell\DatabaseShell::class, 'statusAction']);
-Route::shell('database/create', [App\Controllers\Shell\DatabaseShell::class, 'createAction']);
 Route::shell('database/describe', [App\Controllers\Shell\DatabaseShell::class, 'describeAction']);
+Route::shell('database/tables', [App\Controllers\Shell\DatabaseShell::class, 'tablesAction']);
 Route::shell('database/build-structure', [App\Controllers\Shell\DatabaseShell::class, 'structureAction']);
 Route::shell('database/build-model-fields', [App\Controllers\Shell\DatabaseShell::class, 'fieldsAction']);
 
